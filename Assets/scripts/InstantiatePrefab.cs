6e0f8fc61c6e511bf6378ee9b531e3837b2be4ae
Seb De Carteret <sebsplot@gmail.com>
2023-01-22 19:13:56 +0000

    added the instantiation of the can, the can has a weird trigger collider whihc will be fixed by a raycast later

---
+++ Assets/scripts/InstantiatePrefab.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiatePrefab : MonoBehaviour
+{
+    public GameObject Beans;
+    public int NumberOfCans = 5;
+    public float SpaceBetweenCans = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 xAxis = transform.right;
+        Vector3 yAxis = transform.up * 0.1f;
+        //Vector3 zAxis = transform.forward;
+
+        for (var i = 0; i < NumberOfCans; i++)
+        {
+            float offset = i * SpaceBetweenCans;
+            Vector3 position = transform.position + yAxis + xAxis * offset;
+            Instantiate(Beans, position, transform.rotation);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
