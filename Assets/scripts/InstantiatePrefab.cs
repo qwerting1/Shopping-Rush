a96cf52f8d3a529d7ff195a1ca8c3b27060f6d18
Seb De Carteret <sebsplot@gmail.com>
2023-02-08 21:34:17 +0000

    added pork. Made new offset for z axis, there is now offset for XYZ. Tooltip on the one that is the offset for gap between items.

--- Assets/scripts/InstantiatePrefab.cs
+++ Assets/scripts/InstantiatePrefab.cs
@@ -6,21 +6,26 @@ public class InstantiatePrefab : MonoBehaviour
 {
     public GameObject[] Prefabs;
     public int NumberOfObjects = 36;
-    public float SpaceBetweenObjects = 0.79f;
-    public Quaternion RotationOffset;
-    public float HeightOffset = 0.1f;
+    [Tooltip("Space between objects")]
+    public float XOffset = 0.79f;
+    public float YOffset = 0.1f;
+    public float ZOffset = 0f;
+    public Vector3 RotationOffset;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        Quaternion RotationOffsetQ = Quaternion.Euler(RotationOffset); //convert vector3 to quaternion
         Vector3 xAxis = transform.right;
-        Vector3 yAxis = transform.up * HeightOffset;
+        Vector3 yAxis = transform.up * YOffset;
+        Vector3 zAxis = transform.forward * ZOffset;
 
         for (var i = 0; i < NumberOfObjects; i++)
         {
-            float offset = i * SpaceBetweenObjects;
-            Vector3 position = transform.position + yAxis + xAxis * offset;
-            Quaternion rotation = transform.rotation * RotationOffset;
+            float offset = i * XOffset;
+            Vector4 position = transform.position + yAxis + zAxis + xAxis * offset;
+            Quaternion rotation = transform.rotation * RotationOffsetQ;
 
             int randomIndex = UnityEngine.Random.Range(0, Prefabs.Length);
             GameObject randomPrefab = Prefabs[randomIndex];
